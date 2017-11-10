2583528b61d47aa06e40fd1d0cb449694fe8e19d
Rubentapia <33097861+Rubentapia@users.noreply.github.com>
2017-11-10 15:53:16 -0800

    Update Game.java

--- Mancala/src/Game.java
+++ Mancala/src/Game.java
@@ -1,4 +1,12 @@
 
 public class Game {
+	private int[][] pits = new int [2][6];
+	private boolean isGameOver = false;
+	private final int MaxUndos = 3;
+	private int endPits = new int [2];
+	private ArrayList<ChangeListener> listenerL = new ArrayList<ChangeListener>();
+	private int currentU = 0;
+	
+
 
 }
