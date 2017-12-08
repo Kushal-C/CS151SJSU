8b3bd8075cb623867f1678728ddf281c3011e72d
Kushal Cuttari <kcutari@gmail.com>
2017-12-08 12:25:58 -0800

    Debugging game logic

--- Mancala/src/Board.java
+++ Mancala/src/Board.java
@@ -106,6 +106,17 @@ public class Board implements ImageObserver {
 					@Override
 					public void mouseClicked(MouseEvent e) {
 						model.playGame(pit.getRow(), pit.getCol());
+						int[][] temp = model.getPits();
+						for(int i = 0; i < 2; i++)
+						{
+							for(int j = 0; j < 6; j++)
+							{
+								System.out.print(temp[i][j] + " ");
+							}
+							System.out.println();
+						}
+						addPits();
+						addEndPanels();
 					}
 
 					@Override
